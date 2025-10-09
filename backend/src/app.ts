dd2d20684ccc788cbd737a9c31c1c9810e129a77
OgaitnaSZ <zanty201212@gmail.com>
2025-10-09 16:51:37 -0300

    actualizacion de prisma, limpieza de archivos

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -26,6 +26,8 @@ app.use("/api", routes);
 
 const port = process.env.PORT || 4001;
 const NODE_ENV = process.env.NODE_ENV;
-if(NODE_ENV !== 'test') app.listen(port);
+if(NODE_ENV !== 'test') app.listen(port, ()=>{
+    console.log("Running in: ", process.env.PUBLIC_URL);
+});
 
 export default app;
---
+++ backend/src/test/helper/helperData.ts
@@ -0,0 +1,13 @@
+// Auth
+export const userRegister = {
+    "nombre": "Santiago",
+    "apellido": "Zelaya",
+    "email": "admin@gmail.com",
+    "telefono": "12381923",
+    "password": "admin"
+}
+
+export const userLogin = {
+    "email": "admin@gmail.com",
+    "password": "admin"
+}
